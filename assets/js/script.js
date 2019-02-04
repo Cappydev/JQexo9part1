670060d39345e99fe0c92ceab8a315a7400ec544
cappydev <kevin.lanel@wanadoo.fr>
2019-02-04 19:21:14 +0100

    corrigé exo9

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    // on retire avec le .remove les class <remove>
+    $('#firstText,#thirdText').css('border', '5px green dashed');
+});
---
+++ assets/js/style.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    // On cache le div a afficher
+    $('#text').hide();
+ });
